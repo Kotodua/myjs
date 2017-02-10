6570b844bf428561118a6cdfcb7bc31b06d3497e
Kostiantyn Allakhvierdov <k.allakhvierdov@synapse.com>
2017-02-10 13:56:49 +0200

    added css folder

--- trainings/oop/playing/scripts/main.js
+++ trainings/oop/playing/scripts/main.js
@@ -1,26 +1,46 @@
 (function main(){
-	"use strict"
-
-
-	//setTimeout(funct, 1000);
-
-
-var Some = function(){
-	this.name = 'vasya';
-
-
-	this.getName = function(){
-		console.log(this.name);
+	"use strict";
+	
+var C = function() {
+	this.type = "C"
+	
+	var changeType = function(type){
+		this.type = type;
 	}
+} 
+
+C.prototype.counter = 0;
+
+C.prototype.incr = function(){
+	this.counter++;
 }
 
-
-function show(){
-	var s = new Some();
-
-	s.getName();
+C.prototype.get = function(){
+	return this.type+' '+this.counter;
 }
 
-setTimeout(show, 1000);
+var c = new C();
+
+c.incr();
+console.log(c.get());
+
+
+var D = function(){
+	this.type = "D";
+};
+D.prototype = Object.create(C.prototype);
+D.prototype.constructor = D;
+
+var d = new D();
+
+d.incr();
+d.incr();
+d.incr();
+d.incr();
+console.log(d.get());
+
+
+
+
 
 })()
