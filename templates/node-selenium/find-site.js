9f1c7c70a0becf3bf4b1956dd6e2e55595be3654
Konstantin Allakhvierdov <Konstantin Allakhvierdov>
2017-02-20 14:49:24 +0200

    almost finished find site test

--- templates/node-selenium/find-site.js
+++ templates/node-selenium/find-site.js
@@ -3,16 +3,18 @@
 */
  
 "use strict";
- 
+require('chromedriver'); 
 var webdriver = require('selenium-webdriver');
 var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
- 
+var RegEx = require('regex'); 
+
+
 function logTitle() {
     browser.getTitle().then(function(title) {
         console.log('Current Page Title: ' + title);
     });
 }
- 
+    
 function clickLink(link) {
     link.click();
 }
@@ -23,16 +25,35 @@ function handleFailure(err) {
 }
  
 function findTutsPlusLink() {
-    return browser.findElements(webdriver.By.css('[href="http://code.tutsplus.com/"]')).then(function(result) {
-        return result[0];
+    return browser.findElements(webdriver.By.linkText("Learn How To Code by Envato Tuts+")).then(function(result) {
+        return result;
     });
 }
+
+function getAllResults(){
+    return browser.findElements(webdriver.By.className('r')).then(function(result){
+        result[0].findElement(webdriver.By.tagName('a')).then(function(link){
+            link.getAttribute("href").then(function(linktext){
+                var res = linktext.search("www.macmillandictionary.com");            
+                console.log(linktext);
+            })
+        })
+        result[0].click();
+    });    
+}
+
  
 function closeBrowser() {
     browser.quit();
 }
  
 browser.get('https://www.google.com');
-browser.findElement(webdriver.By.name('q')).sendKeys('tuts+ code');
+browser.findElement(webdriver.By.name('q')).sendKeys('find something');
 browser.findElement(webdriver.By.name('btnG')).click();
-browser.wait(findTutsPlusLink, 2000).then(clickLink).then(logTitle).then(closeBrowser, handleFailure);
+
+setTimeout(getAllResults, 5000);
+
+
+
+
+//browser.wait(findTutsPlusLink, 10000).then(clickLink).then(logTitle).then(closeBrowser, handleFailure);
