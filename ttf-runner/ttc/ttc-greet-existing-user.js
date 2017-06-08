ba78139eb635173d18ad497a3c224291bb4c6f01
Konstantin Allakvierdov <konstantin.allakvierdov@teradek.com>
2017-06-08 16:44:20 +0300

    removed all

--- ttf-runner/ttc/ttc-greet-existing-user.js
+++
@@ -1,9 +0,0 @@
-var common = require("../common");
-var options = common.options;
-var assert = common.assert;
-var expect = common.expect;
-
-it("greet new user", function () {
-   	expect(common.newUser.greeting()).to.equal("Dave");
-});
-
