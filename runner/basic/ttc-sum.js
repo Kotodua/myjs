3da88b030a3455cef1b91bc5ebead71e7c30d9c3
Konstantin Allakhvierdov <Konstantin Allakhvierdov>
2017-06-08 22:08:38 +0300

    updated runner

--- runner/basic/ttc-sum.js
+++ runner/basic/ttc-sum.js
@@ -3,16 +3,12 @@ var expect    = require("chai").expect;
 
 
 exports.run = function(){
+	var posAndPos = calc.methods.sum(4, 2);
+	var negAndNeg  = calc.methods.sum(-4, -1);
+	var negAndPos = calc.methods.sum(4, -2)
+	console.log("4+2=6 is "+posAndPos===6);
+	console.log("-4+(-1)=-5 is "+posAndPos===-5);
+	console.log("4+2=4 is "+posAndPos===4);
 
-	describe("Sum test case", function() {
-	    it("check different numbers sum", function() {
-	      var posAndPos = calc.methods.sum(4, 2);
-	      var negAndNeg  = calc.methods.sum(-4, -1);
-	      var negAndPos = calc.methods.sum(4, -2)
 
-	      expect(posAndPos).to.equal(6);
-	      expect(negAndNeg).to.equal(-5);
-	      expect(negAndPos).to.equal(2);
-	    });
-	});
 }
